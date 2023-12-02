b49a045500dfd5d23bc2f27bfbb88127f8afa465
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-01 23:29:54 -0500

    First commit :3

---
+++ src/Simmons.ts
@@ -0,0 +1,101 @@
+// You can also use the generator at https://skeleton.dev/docs/generator to create these values for you
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+export const Simmons: CustomThemeConfig = {
+	name: 'Simmons',
+	properties: {
+		// =~= Theme Properties =~=
+		'--theme-font-family-base': `system-ui`,
+		'--theme-font-family-heading': `system-ui`,
+		'--theme-font-color-base': '0 0 0',
+		'--theme-font-color-dark': '255 255 255',
+		'--theme-rounded-base': '4px',
+		'--theme-rounded-container': '4px',
+		'--theme-border-base': '2px',
+		// =~= Theme On-X Colors =~=
+		'--on-primary': '0 0 0',
+		'--on-secondary': '255 255 255',
+		'--on-tertiary': '255 255 255',
+		'--on-success': '0 0 0',
+		'--on-warning': '0 0 0',
+		'--on-error': '255 255 255',
+		'--on-surface': '255 255 255',
+		// =~= Theme Colors  =~=
+		// primary | #39a9de
+		'--color-primary-50': '225 242 250', // #e1f2fa
+		'--color-primary-100': '215 238 248', // #d7eef8
+		'--color-primary-200': '206 234 247', // #ceeaf7
+		'--color-primary-300': '176 221 242', // #b0ddf2
+		'--color-primary-400': '116 195 232', // #74c3e8
+		'--color-primary-500': '57 169 222', // #39a9de
+		'--color-primary-600': '51 152 200', // #3398c8
+		'--color-primary-700': '43 127 167', // #2b7fa7
+		'--color-primary-800': '34 101 133', // #226585
+		'--color-primary-900': '28 83 109', // #1c536d
+		// secondary | #8e2c8c
+		'--color-secondary-50': '238 223 238', // #eedfee
+		'--color-secondary-100': '232 213 232', // #e8d5e8
+		'--color-secondary-200': '227 202 226', // #e3cae2
+		'--color-secondary-300': '210 171 209', // #d2abd1
+		'--color-secondary-400': '176 107 175', // #b06baf
+		'--color-secondary-500': '142 44 140', // #8e2c8c
+		'--color-secondary-600': '128 40 126', // #80287e
+		'--color-secondary-700': '107 33 105', // #6b2169
+		'--color-secondary-800': '85 26 84', // #551a54
+		'--color-secondary-900': '70 22 69', // #461645
+		// tertiary | #1a1a1a
+		'--color-tertiary-50': '221 221 221', // #dddddd
+		'--color-tertiary-100': '209 209 209', // #d1d1d1
+		'--color-tertiary-200': '198 198 198', // #c6c6c6
+		'--color-tertiary-300': '163 163 163', // #a3a3a3
+		'--color-tertiary-400': '95 95 95', // #5f5f5f
+		'--color-tertiary-500': '26 26 26', // #1a1a1a
+		'--color-tertiary-600': '23 23 23', // #171717
+		'--color-tertiary-700': '20 20 20', // #141414
+		'--color-tertiary-800': '16 16 16', // #101010
+		'--color-tertiary-900': '13 13 13', // #0d0d0d
+		// success | #27934a
+		'--color-success-50': '223 239 228', // #dfefe4
+		'--color-success-100': '212 233 219', // #d4e9db
+		'--color-success-200': '201 228 210', // #c9e4d2
+		'--color-success-300': '169 212 183', // #a9d4b7
+		'--color-success-400': '104 179 128', // #68b380
+		'--color-success-500': '39 147 74', // #27934a
+		'--color-success-600': '35 132 67', // #238443
+		'--color-success-700': '29 110 56', // #1d6e38
+		'--color-success-800': '23 88 44', // #17582c
+		'--color-success-900': '19 72 36', // #134824
+		// warning | #fddf40
+		'--color-warning-50': '255 250 226', // #fffae2
+		'--color-warning-100': '255 249 217', // #fff9d9
+		'--color-warning-200': '255 247 207', // #fff7cf
+		'--color-warning-300': '254 242 179', // #fef2b3
+		'--color-warning-400': '254 233 121', // #fee979
+		'--color-warning-500': '253 223 64', // #fddf40
+		'--color-warning-600': '228 201 58', // #e4c93a
+		'--color-warning-700': '190 167 48', // #bea730
+		'--color-warning-800': '152 134 38', // #988626
+		'--color-warning-900': '124 109 31', // #7c6d1f
+		// error | #ba2a33
+		'--color-error-50': '245 223 224', // #f5dfe0
+		'--color-error-100': '241 212 214', // #f1d4d6
+		'--color-error-200': '238 202 204', // #eecacc
+		'--color-error-300': '227 170 173', // #e3aaad
+		'--color-error-400': '207 106 112', // #cf6a70
+		'--color-error-500': '186 42 51', // #ba2a33
+		'--color-error-600': '167 38 46', // #a7262e
+		'--color-error-700': '140 32 38', // #8c2026
+		'--color-error-800': '112 25 31', // #70191f
+		'--color-error-900': '91 21 25', // #5b1519
+		// surface | #595959
+		'--color-surface-50': '230 230 230', // #e6e6e6
+		'--color-surface-100': '222 222 222', // #dedede
+		'--color-surface-200': '214 214 214', // #d6d6d6
+		'--color-surface-300': '189 189 189', // #bdbdbd
+		'--color-surface-400': '139 139 139', // #8b8b8b
+		'--color-surface-500': '89 89 89', // #595959
+		'--color-surface-600': '80 80 80', // #505050
+		'--color-surface-700': '67 67 67', // #434343
+		'--color-surface-800': '53 53 53', // #353535
+		'--color-surface-900': '44 44 44' // #2c2c2c
+	}
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import i3_video from '$lib/assets/i3.mp4';
+	import i3_thumbnail from '$lib/assets/i3_thumbnail.png';
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="card p-4 flex flex-col space-y-4">
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video class="h-full w-full rounded-md max-w-4xl" controls poster={i3_thumbnail}>
+			<source src={i3_video} type="video/mp4" />
+		</video>
+
+		<a href="/prospectives/" class="btn variant-filled-primary" data-sveltekit-preload-data="hover">
+			Welcome Prospectives! (+ More videos)
+		</a>
+	</div>
+</div>
