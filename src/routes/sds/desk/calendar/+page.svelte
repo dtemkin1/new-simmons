ce9d00c625a589acd4e22bf419404d3cdd660e3e
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-03 18:24:21 -0500

    make calendar rounded

--- src/routes/sds/desk/calendar/+page.svelte
+++ src/routes/sds/desk/calendar/+page.svelte
@@ -1,5 +1,5 @@
 <iframe
 	src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showTz=1&mode=WEEK&src=a24xMGpscGxhMWhlNzRrN2I1YXJkdWg1ZzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5"
-	class="w-full h-full border-none"
+	class="w-full h-full border-none rounded-container-token"
 	title="Desk Shift Calendar"
 ></iframe>
