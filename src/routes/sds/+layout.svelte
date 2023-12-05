6a5de30ee68b1c2b175e7dbcd21f99b90f6c3e04
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-04 21:36:58 -0500

    remove console logging

--- src/routes/sds/+layout.svelte
+++ src/routes/sds/+layout.svelte
@@ -1,12 +1,3 @@
-<script lang="ts">
-	import { getContext } from 'svelte';
-
-	let currentTile = getContext('currentTile');
-	console.log(currentTile);
-
-	// TODO: Implement submenus
-</script>
-
 <div class="bg-surface-100-800-token h-full">
 	<div class="bg-surface-50-900-token h-full md:!block rounded-container-token">
 		<slot />
