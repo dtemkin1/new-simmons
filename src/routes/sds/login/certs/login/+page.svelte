b85800761b970ca9c6e0980c5723facdf3286113
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-05 19:42:29 -0500

    update sign-in

--- src/routes/sds/login/certs/login/+page.svelte
+++ src/routes/sds/login/certs/login/+page.svelte
@@ -3,30 +3,39 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="h1">Welcome to Simmons DB</h1>
-<p>
-	{#if $page.data.session}
-		<!-- {#if $page.data.session.user?.image}
+<div class="flex items-center justify-center h-full">
+	<div class="card p-8 flex flex-col space-y-4 max-w-5xl m-8">
+		{#if $page.data.session}
+			<!-- {#if $page.data.session.user?.image}
 			<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
 		{/if} -->
-		<span class="signedInText">
-			<p>
-				You are currently logged in as <span class="text-strong"
-					>{$page.data.session.user?.email ?? 'User'}</span
-				>.
-			</p>
-		</span>
-		<br />
-		<button type="button" class="btn variant-filled-error" on:click={() => signOut()}
-			>Sign out</button
-		>
-	{:else}
-		<span class="notSignedInText"
-			>You are not signed in. Please sign in to access the Simmons DB.</span
-		>
-		<br />
-		<button type="button" class="btn variant-filled-success" on:click={() => signIn('petrock')}
-			>Sign In with Touchstone</button
-		>
-	{/if}
-</p>
+			<span class="signedInText">
+				<p>
+					You are currently logged in as <span class="text-strong"
+						>{$page.data.session.user?.email ?? 'User'}</span
+					>.
+				</p>
+			</span>
+			<button type="button" class="btn variant-filled-error" on:click={() => signOut()}
+				>Sign out</button
+			>
+		{:else}
+			<h2 class="h2">Sign in to Simmons DB</h2>
+			<label class="label">
+				<span>Username</span>
+				<input class="input" title="username" type="text" />
+			</label>
+			<label class="label">
+				<span>Password</span>
+				<input class="input" title="password" type="password" />
+			</label>
+			<button type="button" class="btn variant-filled" on:click={() => signIn('credentials')}
+				>Sign In with Credentials</button
+			>
+			<hr />
+			<button type="button" class="btn variant-filled-success" on:click={() => signIn('petrock')}
+				>Sign In with Touchstone</button
+			>
+		{/if}
+	</div>
+</div>
