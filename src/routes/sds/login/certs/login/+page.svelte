2ae0db2f1a6f7354524fc511ba226ab9c4febc87
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-05 21:26:29 -0500

    debug again sobs

--- src/routes/sds/login/certs/login/+page.svelte
+++ src/routes/sds/login/certs/login/+page.svelte
@@ -12,7 +12,7 @@
 			<span class="signedInText">
 				<p>
 					You are currently logged in as <span class="text-strong"
-						>{$page.data.session.user?.name ?? 'User'}</span
+						>{$page.data.session.user?.id ?? 'User'}</span
 					>.
 				</p>
 			</span>
