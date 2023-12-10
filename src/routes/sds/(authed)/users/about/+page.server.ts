b95e0d99e073e57ba56c15b72b96826b924740e9
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-09 22:36:21 -0500

    finalize about page

--- src/routes/sds/(authed)/users/about/+page.server.ts
+++ src/routes/sds/(authed)/users/about/+page.server.ts
@@ -1,13 +1,10 @@
-// import { connectToDB } from '$lib/db';
+import { connectToDB } from '$lib/db';
 import type { PageServerLoad } from './$types';
 
-// TODO: REPLACE WITH ACTUAL FUNCTION
 export const load: PageServerLoad = async () => {
-	return { dbName: 'sdb', version: '8.3.5' };
-};
-// export const load: PageServerLoad = async () => {
-// 	const db = await connectToDB();
-// 	const version = (await db.query('SELECT VERSION()')).rows[0];
+	const db = await connectToDB();
+	const version = (await db.query("SELECT split_part(version(),' on ',1)")).rows[0].split_part;
+	const dnName = (await db.query('SELECT current_database()')).rows[0].current_database;
 
-// 	return { dbName: 'test', version: version };
-// };
+	return { dbName: dnName, version: version };
+};
