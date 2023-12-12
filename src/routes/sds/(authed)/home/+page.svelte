6e971feef28a9d2b99d413924e0a97d620cd0ecb
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-12 00:04:25 -0500

    change db main text

--- src/routes/sds/(authed)/home/+page.svelte
+++ src/routes/sds/(authed)/home/+page.svelte
@@ -27,34 +27,36 @@
 				friendly, teddy-bear-like guide to all things simmons-tech.
 			</p>
 
-			<p>
+			<!-- <p>
 				Summer has begun and REX is coming ever-closer! If you have an idea for an event you think
 				Simmons should run, you should submit it <a
 					class="anchor"
 					href="https://forms.gle/qnE88D5ysMRQ7jpH9">here</a
 				> by next Tuesday, May 31!
-			</p>
+			</p> -->
 
 			<p>
-				<span class="font-bold">Important note:</span> If you're unfamiliar with the new GovTracker system
+				<span class="font-bold">Important note:</span> If you're unfamiliar with new GovTracker system
 				for managing the house's and lounges' proposals and finances, please ask an officer or upperclassmen
 				before submitting anything. In general, submitting data to any GovTracker system cannot be undone.
 			</p>
 
 			<p>
-				The Simmons DB is set up to use <a class="font-bold anchor" href="http://ca.mit.edu/"
-					>MIT Certificates</a
-				>
+				The Simmons DB is set up to use <a
+					class="font-bold anchor"
+					href="https://ist.mit.edu/touchstone"
+					>Touchstone Authentication
+				</a>
 				to authenticate logins, but you can also set up the system to let you
 				<a class="anchor" href="{base}/sds/users/password">log in with a password</a> as an alternative.
-				(Note, however, that you need to be logged in by certificate in order to set up your password.)
+				(Note, however, that you need to be already logged in in order to set up your password.)
 			</p>
 
-			<p>
+			<!-- <p>
 				A new feature of the Simmons DB is the ability to login using your MIT Kerberos (email)
 				username and password. To do this, go to the
 				<a class="anchor" href="{base}/sds/login/certs/login">Simmons Login Page</a>
-			</p>
+			</p> -->
 		</div>
 	</div>
 </div>
