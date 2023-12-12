4e8c2df414208b07d70150203640509d1c9fedf3
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-12 14:10:45 -0500

    add type checking

--- src/routes/sds/(authed)/home/+page.server.ts
+++ src/routes/sds/(authed)/home/+page.server.ts
@@ -1,10 +1,35 @@
 import { pool } from '$lib/db';
-import { sql } from 'slonik';
+import { createSqlTag } from 'slonik';
+import { z } from 'zod';
 import type { PageServerLoad } from './$types';
+
+const sql = createSqlTag({
+	typeAliases: {
+		id: z.object({
+			id: z.number()
+		}),
+		void: z.object({}).strict(),
+		resident: z.object({
+			username: z.string(),
+			lastname: z.string(),
+			firstname: z.string(),
+			title: z.string().nullable(),
+			year: z.number().nullable(),
+			type: z.string(),
+			quote: z.string(),
+			favorite_category: z.string(),
+			favorite_value: z.string(),
+			homepage: z.string(),
+			home_city: z.string(),
+			home_state: z.string(),
+			home_country: z.string()
+		})
+	}
+});
 
 export const load: PageServerLoad = async () => {
 	const dbResult = await pool.connect(async (connection) => {
-		const residentsQuery = connection.query(sql.unsafe`
+		const residentsQuery = connection.query(sql.typeAlias('resident')`
 	SELECT username,lastname,firstname,title,year,type,quote,favorite_category,
 		   favorite_value,homepage,home_city,home_state,home_country
 	FROM public_active_directory
--- src/routes/sds/(authed)/users/about/+page.server.ts
+++ src/routes/sds/(authed)/users/about/+page.server.ts
@@ -1,22 +1,53 @@
 import { pool } from '$lib/db';
-import { sql } from 'slonik';
+import { createSqlTag } from 'slonik';
 import type { PageServerLoad } from './$types';
+import { z } from 'zod';
+
+const sql = createSqlTag({
+	typeAliases: {
+		id: z.object({
+			id: z.number()
+		}),
+		void: z.object({}).strict(),
+		user: z.object({
+			username: z.string(),
+			lastname: z.string(),
+			firstname: z.string()
+		}),
+		userNoUsername: z.object({
+			lastname: z.string(),
+			firstname: z.string()
+		})
+	}
+});
 
 export const load: PageServerLoad = async () => {
 	const dbResult = await pool.connect(async (connection) => {
-		const versionQuery = connection.query(sql.unsafe`SELECT split_part(version(),' on ',1)`);
-		const dbNameQuery = connection.query(sql.unsafe`SELECT current_database()`);
+		const versionQuery = connection.query(
+			sql.type(z.object({ split_part: z.string() }))`SELECT split_part(version(),' on ',1)`
+		);
+		const dbNameQuery = connection.query(
+			sql.type(z.object({ current_database: z.string() }))`SELECT current_database()`
+		);
 		const adminsQuery = connection.query(
-			sql.unsafe`SELECT username,lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='ADMINISTRATORS' ORDER BY lastname ASC`
+			sql.typeAlias(
+				'user'
+			)`SELECT username,lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='ADMINISTRATORS' ORDER BY lastname ASC`
 		);
 		const modsQuery = connection.query(
-			sql.unsafe`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='MODERATORS' ORDER BY lastname ASC`
+			sql.typeAlias(
+				'userNoUsername'
+			)`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='MODERATORS' ORDER BY lastname ASC`
 		);
 		const housecommLeadershipQuery = connection.query(
-			sql.unsafe`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='HOUSE-COMM-LEADERSHIP' ORDER BY lastname ASC`
+			sql.typeAlias(
+				'userNoUsername'
+			)`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='HOUSE-COMM-LEADERSHIP' ORDER BY lastname ASC`
 		);
 		const financialAdminsQuery = connection.query(
-			sql.unsafe`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='FINANCIAL-ADMINS' ORDER BY lastname ASC`
+			sql.typeAlias(
+				'userNoUsername'
+			)`SELECT lastname,firstname FROM sds_group_membership_cache JOIN directory USING (username) WHERE groupname='FINANCIAL-ADMINS' ORDER BY lastname ASC`
 		);
 
 		const [version, dbName, admins, mods, housecommLeadership, financialAdmins] = await Promise.all(
