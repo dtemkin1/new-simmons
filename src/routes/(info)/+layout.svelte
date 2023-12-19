3ebcbb9af506449abfe33df1bc320d702ce92a28
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-19 16:21:18 -0500

    improve legibility

--- src/routes/(info)/+layout.svelte
+++ src/routes/(info)/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="card p-8 flex flex-col space-y-4 max-w-5xl m-8">
+	<div class="card p-8 flex flex-col space-y-4 max-w-5xl m-8 bg-surface-50-900-token">
 		<slot />
 	</div>
 </div>
