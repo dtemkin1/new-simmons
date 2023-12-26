4edb43ea0d2bf794b1286a5935b8f6e4e9679b25
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-26 16:43:53 -0600

    remove unneeded import

--- src/routes/sds/(authed)/directory/+page.svelte
+++ src/routes/sds/(authed)/directory/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { base } from '$app/paths';
 	import DirectorySearch from '$lib/components/DirectorySearch.svelte';
 
 	export let data: PageData;
