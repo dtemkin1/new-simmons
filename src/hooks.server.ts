4c6b2a22388628fcd473de1b935dfbcebf3f10cb
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-25 21:36:48 -0600

    fix env variables again

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,8 @@ import {
 	CLIENT_ID,
 	CLIENT_SECRET,
 	AUTH_SECRET,
-	AUTH_REDIRECT_PROXY_URL,
-	AUTH_TRUST_HOST
-} from '$env/static/private';
+	AUTH_REDIRECT_PROXY_URL
+} from '$env/dynamic/private';
 
 import { pool } from '$lib/db';
 import { createSqlTag } from 'slonik';
@@ -85,7 +84,6 @@ const authHandle = SvelteKitAuth({
 	providers: [petrockProvider],
 	secret: AUTH_SECRET,
 	session: { strategy: 'jwt' },
-	trustHost: AUTH_TRUST_HOST == 'true',
 	callbacks: {
 		async jwt({ token, user }) {
 			if (user) {
