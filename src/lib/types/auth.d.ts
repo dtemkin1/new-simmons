43bc256f9be84e12c1b3972fbaa3b739b0d9df3f
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-01-06 08:20:56 -0500

    update types

--- src/lib/types/auth.d.ts
+++ src/lib/types/auth.d.ts
@@ -1,10 +1,9 @@
-import { DefaultSession } from '@auth/sveltekit/types';
+import type { User } from '@auth/sveltekit/types';
 
 declare module '@auth/core/types' {
 	interface Session {
 		user: {
-			id: string;
-			groups: readonly string[];
-		} & DefaultSession['user'];
+			groups?: readonly string[];
+		} & User;
 	}
 }
