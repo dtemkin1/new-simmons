b15232fe0ee54324a2edabac18151fc820cdefd2
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-01-11 14:00:54 -0500

    better error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
+
+	let inDB = $page.url.pathname.includes(`${base}/sds`);
 </script>
 
-{#if !$page.url.pathname.includes(`${base}/sds`)}
+{#if !inDB}
 	<div class="container h-full mx-auto flex justify-center items-center">
-		<div class="card p-8 flex flex-col space-y-4 max-w-5xl m-8 bg-surface-50-900-token">
+		<div class="card p-8 flex flex-col space-y-2 max-w-5xl m-8 bg-surface-50-900-token">
+			<h1 class="h1">
+				{$page.status} Error: {$page.error?.message}
+				<span class="text-surface-500-400-token">😔</span>
+			</h1>
 			{#if $page.status === 404}
-				<h1 class="h1">Not found <span class="text-surface-500-400-token">:(</span></h1>
 				<p>Sorry, but the page you were trying to view does not exist.</p>
 				<p>It looks like this was the result of either:</p>
 				<ul class="list-disc list-outside ml-6 py-2">
 					<li>a mistyped address</li>
 					<li>an out-of-date link</li>
 				</ul>
-			{:else if $page.error}
-				<h1>{$page.status}: {$page.error.message}</h1>
 			{/if}
 			<p>
 				Please <a class="anchor" href="mailto:simmons-tech@mit.edu">contact the Tech Chairs</a> to report
 				this incident.
 			</p>
+			<img
+				src="https://http.cat/{$page.status}"
+				alt="Cat Error {$page.status} Picture"
+				class="max-w-lg"
+			/>
 		</div>
 	</div>
 {:else}
 	<div
-		class="bg-surface-50-900-token flex flex-col items-center p-4 justify-center h-full align-center gap-4"
+		class="bg-surface-50-900-token flex flex-col items-center p-4 justify-center h-full align-center gap-2"
 	>
+		<h1 class="h1 text-center">
+			{$page.status} Error: {$page.error?.message}
+			<span class="text-surface-500-400-token">😔</span>
+		</h1>
 		{#if $page.status === 404}
-			<h1 class="h1 text-center">Not found <span class="text-surface-500-400-token">:(</span></h1>
 			<p class="text-center">Sorry, but the page you were trying to view does not exist.</p>
 			<p class="text-center">It looks like this was the result of either:</p>
 			<ul class="list-disc list-outside ml-6 py-2">
 				<li>a mistyped address</li>
 				<li>an out-of-date link</li>
 			</ul>
-		{:else if $page.error}
-			<h1>{$page.status}: {$page.error.message}</h1>
 		{/if}
 		<p class="text-center">
 			Please <a class="anchor" href="mailto:simmons-tech@mit.edu">contact the Tech Chairs</a> to
 			report this incident, and
 			<a class="anchor" href="{base}/sds">click here</a> to return to the Simmons DB.
 		</p>
+		<img
+			src="https://http.cat/{$page.status}"
+			alt="Cat Error {$page.status} Picture"
+			class="max-w-lg"
+		/>
 	</div>
 {/if}
