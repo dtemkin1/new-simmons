777a7bf9772d6060cd7263728b1620096e4dc455
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-01-25 23:41:31 -0500

    remove work in progress tag

--- src/lib/data/navLinks.ts
+++ src/lib/data/navLinks.ts
@@ -178,7 +178,7 @@ export const sdsLinks: SDSGroups[] = [
 			{
 				href: `${SDS_BASE}/directory/update`,
 				label: 'My Profile',
-				badge: 'Work in Progress',
+				badge: '',
 				groupNeeded: ['USERS']
 			},
 			{
