d3d3aed038c1e88cf2b63ad4d201d1e533bed688
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-06-12 21:08:31 -0400

    add video poster

--- src/routes/(info)/+page.svelte
+++ src/routes/(info)/+page.svelte
@@ -3,13 +3,18 @@
 <script lang="ts">
 	import i3_video from '$lib/assets/i3.mp4';
 	// import i3_captions from '$lib/assets/i3_captions.vtt';
-	// import i3_thumbnail from '$lib/assets/i3_thumbnail.avif';
+	import i3_thumbnail from '$lib/assets/i3_thumbnail.jpg';
 
 	import { base } from '$app/paths';
 </script>
 
 <!-- svelte-ignore a11y_media_has_caption -->
-<video class="h-full w-full rounded-container-token max-w-[720px]" preload="none" controls>
+<video
+	class="h-full w-full rounded-container-token max-w-[720px]"
+	preload="none"
+	poster={i3_thumbnail}
+	controls
+>
 	<source src={i3_video} type="video/mp4" />
 	<!-- <track default kind="captions" src={i3_captions} srclang="en" label="English" /> -->
 </video>
