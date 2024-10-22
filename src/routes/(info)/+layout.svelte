5b9343880c7b9bb7c71a65ee21bd8885fa930cfd
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-10-22 19:45:19 -0400

    use enhanced img for background

--- src/routes/(info)/+layout.svelte
+++ src/routes/(info)/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	let { children } = $props();
+
+	import backgroundImg from '$lib/assets/bg.jpg?enhanced';
 </script>
 
-<div class="h-full w-full img-bg">
+<div class="h-full w-full img-bg" style="--background-image: url({backgroundImg.img.src});">
 	<div class="container h-full mx-auto flex justify-center items-center">
 		<div class="card p-8 m-8 space-y-4 max-w-5xl bg-surface-50-900-token">
 			{@render children()}
 		</div>
 	</div>
 </div>
+
+<style>
+	:global(.img-bg) {
+		background-image: var(--background-image);
+		background-attachment: fixed;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+</style>
